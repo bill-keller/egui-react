556ce42b8de1a82805a808801a88e73a98a53dde
Bill Keller <bkeller99@gmail.com>
2019-06-07 07:51:08 -0500

    updated Login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({startLogin}) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify</h1>
-        <p>It's time to get your expenses under control.</p>
+      <h1 className="box-layout__title">Mobile Device Inventory</h1>
+        <p>Let's see what mainframe data looks like now.</p>
       <button onClick={startLogin} className="button">Login with Google</button>
     </div>
   </div>
