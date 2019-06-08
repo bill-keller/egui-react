b8eb3d559ecb4ca731bcb88165da1d2200499678
Bill Keller <bkeller99@gmail.com>
2019-06-08 10:23:11 -0500

    updated device total summary

--- src/components/DevicesSummary.js
+++ src/components/DevicesSummary.js
@@ -7,11 +7,12 @@ import selectDevices from '../selectors/devices';
 
 const DevicesSummary = ({deviceCount, devicesTotal}) => {
   const deviceWord = deviceCount === 1 ? 'device' : 'devices';  
-  const formattedDeviceTotal = numeral(devicesTotal / 100).format('$0,0.00')
+  //const formattedDeviceTotal = numeral(devicesTotal).format('$0,0.00')
+  const formattedDeviceTotal = numeral(0).format('$0,0.00')
   return (
   <div className="page-header">
     <div className="content-container">
-      <h1 className="page-header__title">Cart contains <span>{deviceCount} devices</span> totalling: <span>{formattedDeviceTotal}</span></h1>
+      <h1 className="page-header__title">Cart contains <span>0 devices</span> totalling: <span>{formattedDeviceTotal}</span></h1>
       <div className="page-header_actions">
         <Link className="button" to ="/dashboard">Add Device</Link>
       </div>
@@ -20,7 +21,9 @@ const DevicesSummary = ({deviceCount, devicesTotal}) => {
   </div>
 )};
 
-        // <Link className="button" to ="/create">Add Device</Link> 
+      // <h1 className="page-header__title">Cart contains <span>{deviceCount} devices</span> totalling: <span>{formattedDeviceTotal}</span></h1>
+
+      // <Link className="button" to ="/create">Add Device</Link> 
 
 const mapStateToProps = (state) => {
   const visibleDevices = selectDevices(state.devices, state.filters);
